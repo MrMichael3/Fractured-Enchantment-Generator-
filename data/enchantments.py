3bdfff28383653ce0d684d17012dbe3c16fac575
Michael <michael.kuechler2@uzh.ch>
2020-08-02 16:51:26 +0200

    Add files via upload

---
+++ data/enchantments.py
@@ -0,0 +1,5 @@
+# list of properties
+
+properties_list = ["Mind", "Body", "Soul", "Create", "Destroy", "Negate", "Transform", "Transfer", "Air", "Earth",
+                   "Fire", "Water", "Death", "Life", "Chaos", "Order", "Time", "Energy", "Knowledge"]
+
